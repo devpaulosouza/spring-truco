a45a8d1a627cd590e6ccb8837dd1b27667ac4657
Paulo Souza <paulo.souza@petzpartner.com.br>
2024-08-19 21:06:59 -0300

    feat: initial configuration

---
+++ src/main/java/dev/paulosouza/truco/TrucoApplication.java
@@ -0,0 +1,13 @@
+package dev.paulosouza.truco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrucoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrucoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/paulosouza/truco/TrucoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.paulosouza.truco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrucoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
